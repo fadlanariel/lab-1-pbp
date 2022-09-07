fd81bbf5b75cff0a4e2a0757c8b8bf7c73995445
Fadlan Ariel Fathurrahman <fadlan.ariel@ui.ac.id>
2022-09-07 19:32:58 +0700

    menghubungkan views dan models

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from wishlist.models import BarangWishlist
+
+data_barang_wishlist = BarangWishlist.objects.all()
+context = {
+    'list_barang': data_barang_wishlist,
+    'nama': 'Fadlan Ariel'   
+}
 
 # Create your views here.
 def show_wishlist(request):
-    return render(request, "wishlist.html")
+    return render(request, "wishlist.html", context)
+
+
